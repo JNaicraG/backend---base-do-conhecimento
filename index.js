cad7d7ac3d9e71a5c5011a8af983b7db8509361e
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-03-05 20:06:27 -0300

    feat: routes.js created

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const app = require('express')();
 const consign = require('consign');
 
-consign()
+consign() //Ele lê os arquivos de config em separado para maior controle - primeiro os middlewares, depois as apis que serão usadas em rotas, e aí rotas
     .then('./config/middlewares.js') //Local do middlware
+    .then('./api') //Lê todos os arquvios em api
+    .then('./config/routes.js') 
     .into(app) //injeta em cada uma das dependecias carregadas o app
 
 app.listen(3000, ()=>{
