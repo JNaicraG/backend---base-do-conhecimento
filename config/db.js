f5ef636252b524e86cb9d3fec15ec11abb984276
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-03-05 22:05:38 -0300

    chore: implemented migrations

--- config/db.js
+++ config/db.js
@@ -1,4 +1,8 @@
 const config = require('../knexfile.js');
 const knex = require('knex')(config);
 
+//Não é a melhor opção toda hora
+//knex.migrate.latest(config);  //Ele executa a migração no momento que se executa o backend
+//Evoluir o db é algo delicado/+controlado
+
 module.exports = knex;
--- migrations/20240305201902_create_table_users.js
+++ migrations/20240305201902_create_table_users.js
@@ -1,8 +1,20 @@
-// npx knex migrate:make create_table_users
-exports.up = function(knex, Promise) {
-  
+// npx knex migrate:make create_table_users p/ criar migrations
+//npx knex migrate:latest pra atualizar as migrations com o banco de dados
+//npx knex migrate:rollback ele dá down
+exports.up = function(knex, Promise) { //Banco de dados "evolui" - cria table, altera tablea, exclui, etc. Vai para versões mais novas
+  //O return nós dá uma maior visualização do que está sendo feito para debug
+  return knex.schema.createTable('users', table =>{
+    table.increments('id').primary();
+    table.string('name').notNull();
+    table.string('email').notNull().unique();
+    table.string('password').notNull();
+    table.boolean('admin').notNull().defaultTo(false);
+  });
+
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function(knex, Promise) { //Desfaz o que foi feito no up
+    //Tem que ser feito o exato oposto do método up
+    return knex.schema.dropTable('users');
   
 };
