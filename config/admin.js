9875e2e35742be3c1ce5f989b5d17a47cc74fd19
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-03-11 15:43:53 -0300

    chore: updated and  implemented admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,1 +1,11 @@
- 
+ module.exports = middleware =>{ //Recebe um middleware como param de entrada. Também retorna um middleware
+    //Não será usada pelo cosign, mas sim importada diretamente
+    return (req,res,next) => {
+        if(req.user.admin) { //se for admin, passar
+            middleware(req,res,next);
+        } else {
+            res.status(402).send('Usuário não é administrador');
+        }
+    }
+
+ }
--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,5 @@
+const admin = require('./admin') //passado como admin(app.api.xyz.abc) por exemplo
+
 //const user = require('../api/user') //Não precisa pois, devido ao cosign, essa dependência já se encontra em app
 module.exports = app => {
 
@@ -9,46 +11,46 @@ module.exports = app => {
     app.post('/signin', app.api.auth.signIn);
     app.post('/validateToken', app.api.auth.validateToken);
 
-
+    //por essa url precisa ser admin nessa app
     app.route('/users') //Associar a endpoint com o arquivo
         .all(app.config.passport.authenticate()) //todos os verbos http passarão por esse filtro e somente chamará os verbos abaixo se o retorno for verdadeiro
-        .post(app.api.user.save) //app.pasta.arquivo.função
-        .get(app.api.user.get);
+        .post(admin(app.api.user.save)) //app.pasta.arquivo.função
+        .get(admin(app.api.user.get));
 
     app.route('/users/:id')
         .all(app.config.passport.authenticate())
-        .put(app.api.user.save)
-        .get(app.api.user.getById);
+        .put(admin(app.api.user.save))
+        .get(admin(app.api.user.getById));
 
     app.route('/categories')
         .all(app.config.passport.authenticate())
-        .get(app.api.category.get)
-        .post(app.api.category.save);
+        .get(admin(app.api.category.get))
+        .post(admin(app.api.category.save));
 
 
     //tem que vir antes de :id para não haver erros e entender /tree como id
     app.route('/categories/tree')
         .all(app.config.passport.authenticate())
-        .get(app.api.category.getTree);
+        .get(app.api.category.getTree); ///usuário pega categoria por aqui
 
     app.route('/categories/:id')
         .all(app.config.passport.authenticate())
         .get(app.api.category.getById)
-        .delete(app.api.category.remove)
-        .put(app.api.category.save);
+        .delete(admin(app.api.category.remove))
+        .put(admin(app.api.category.save));
 
     app.route('/articles')
         .all(app.config.passport.authenticate())
-        .get(app.api.article.get)
-        .post(app.api.article.save);
+        .get(admin(app.api.article.get)) //método pro admin ver
+        .post(admin(app.api.article.save));
 
     app.route('/articles/:id')
         .all(app.config.passport.authenticate())
-        .put(app.api.article.save)
-        .delete(app.api.article.remove)
-        .get(app.api.article.getById);
+        .get(app.api.article.getById)
+        .put(admin(app.api.article.save))
+        .delete(admin(app.api.article.remove));
 
     app.route('/categories/:id/articles')
         .all(app.config.passport.authenticate())
-        .get(app.api.article.getByCategory);
+        .get(app.api.article.getByCategory); //método pro usuário ver
 }
