fbbe4994290de4ad9b7c16da5f00eb731a1b1bbd
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-03-15 03:03:36 -0300

    fix: article to articles in stat.js

--- api/stat.js
+++ api/stat.js
@@ -3,7 +3,7 @@ module.exports = app => {
 
     const statSchema = new app.mongoose.Schema({ //Como será a "tabela"/documento
         users: Number,
-        article: Number,
+        articles: Number,
         categories: Number,
         createdAt: Date
     })
@@ -19,9 +19,6 @@ module.exports = app => {
     //});
 
     const get = (req, res) => {
-        //"Pegar um"
-        //Filtrar por //Selecionar atributo tipoX //Ordenação por "-1" (desc)
-        //Pegar a última estatística adicionada no mongodb (pegar uma única instância do banco ordenado de maneira desc)
         Stat.findOne({}, {}, { sort: { 'createdAt': -1 } })
             .then(stat => {
                 const defaultStat = {
@@ -33,25 +30,6 @@ module.exports = app => {
                 res.json(stat || defaultStat)
             });
     }
-
-    //const Stat = app.mongoose.model('Stat', {
-    //    users: Number,
-    //    categories: Number,
-    //    articles: Number,
-    //    createdAt: Date
-    //})
-
-    //const get = (req, res) => {
-    //    Stat.findOne({}, {}, { sort: { 'createdAt' : -1 } })
-    //        .then(stat => {
-    //            const defaultStat = {
-    //                users: 0,
-    //                categories: 0,
-    //                articles: 0
-    //            }
-    //            res.json(stat || defaultStat)
-    //        })
-    //}
 
     return { get, Stat }
 }
